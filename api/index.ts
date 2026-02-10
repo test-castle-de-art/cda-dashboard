7bc05d64d0068501caa138864f214a8a8fcfc7ef
alexkasiot <alex_kasiot@hotmail.com>
2026-02-10 17:12:05 +0100

    extra attempts to fix vercel build errors

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../server.js"
 
-export default async (request: any, response: any) =>
+export default async function handler(request: VercelRequest, response: VercelResponse)
 {
     await app.ready();
     app.server.emit("request", request, response);
