9ef460cdb56321fe3443c6d04a6639f53ce2d1fe
alexkasiot <alex_kasiot@hotmail.com>
2026-02-10 18:20:32 +0100

    still trying vercel typescript compiling fixes+++++

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Build frontend with Vite (uses tsconfig.app.json correctly)
-npm run vite build
-
-# Build server with YOUR tsconfig
-npx tsc -p tsconfig.server.json --noEmit false --outDir dist
-
-# Tell Vercel we're done
+npm run build:server
+npx vite build --outDir dist/src
 echo "Build complete"
